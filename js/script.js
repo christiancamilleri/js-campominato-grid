0006e2a85c7957aae3f0d3fb0c03cdeac1dc40be
christiancamilleri <camilleri.christian@outlook.com>
2023-03-03 16:30:19 +0100

    feat: bonus complete

--- js/script.js
+++ js/script.js
@@ -8,9 +8,17 @@ Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed 
 // recupero il bottone dall html
 const playButtonEl = document.getElementById("play-button");
 
-// creo evento alla pressione del bottone
+
+
+// recupero scelta difficolta
+let sceltaEl = document.getElementById("scelta");
+
+
+// // creo evento alla pressione del bottone
 playButtonEl.addEventListener("click", function() {
-    const containerEl = document.getElementById("container");
+   const containerEl = document.getElementById("container");
+
+if(sceltaEl.value == "easy") {
     
     // dico al programma di non creare altri schemi uno stto l ltro ogni volta che premo il bottone
     containerEl.innerHTML = ""
@@ -29,7 +37,56 @@ playButtonEl.addEventListener("click", function() {
             newSquareEl.classList.toggle("blue");
             console.log(i);
         })
+       newSquareEl.innerHTML = i
+
     }
 
+}  else if (sceltaEl.value == "medium") {
+    
+    // dico al programma di non creare altri schemi uno stto l ltro ogni volta che premo il bottone
+    containerEl.innerHTML = ""
+    
+    // creo ciclo for che mi crea 100 elementi
+    for(let i = 1; i < 82; i++){
+        // creo elemeno div
+        let newSquareEl = document.createElement("div");
+        // assegno classe creata in css
+        newSquareEl.classList.add("medium");
+        // appendo al genitore
+        containerEl.append(newSquareEl)
 
-})
+        // aggiungo evento alla pressione di NewSquareEl
+        newSquareEl.addEventListener("click", function() {
+            newSquareEl.classList.toggle("blue");
+            console.log(i);
+        })
+        newSquareEl.innerHTML = i
+
+    }
+
+} else if (sceltaEl.value == "hard") {
+        
+        // dico al programma di non creare altri schemi uno stto l ltro ogni volta che premo il bottone
+        containerEl.innerHTML = ""
+        
+        // creo ciclo for che mi crea 100 elementi
+        for(let i = 1; i < 50; i++){
+            // creo elemeno div
+            let newSquareEl = document.createElement("div");
+            // assegno classe creata in css
+            newSquareEl.classList.add("hard");
+            // appendo al genitore
+            containerEl.append(newSquareEl)
+    
+            // aggiungo evento alla pressione di NewSquareEl
+            newSquareEl.addEventListener("click", function() {
+                newSquareEl.classList.toggle("blue");
+                
+                console.log(i);
+            })
+            newSquareEl.innerHTML = i
+
+        }
+       
+    
+    }})
